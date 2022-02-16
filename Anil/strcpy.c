6d9e25acfbbec1436804ed6251e364624fb9d3cd
anii005 <aniigupta98118@gmail.com>
2022-02-16 11:14:07 +0530

    copy the string without using strcpy() file added

---
+++ Anil/strcpy.c
@@ -0,0 +1,19 @@
+//copy the string without using strcpy()
+
+#include <stdio.h>
+
+int main()
+{
+    char s1[100], s2[100],i;          
+    printf("Enter string s1:\n");     
+    fgets(s1, 100, stdin);           
+   
+    for(i = 0; s1[i] != '\0'; i++)   
+    {
+        s2[i] = s1[i];
+    }
+    
+    s2[i] = '\0';                   
+    printf("String s2: %s", s2);
+    return 0;
+}
