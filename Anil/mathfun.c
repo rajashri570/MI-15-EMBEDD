e0b0e6f8d735a362a7152e3d6bef81b8e6df0f0d
anii005 <aniigupta98118@gmail.com>
2022-02-17 10:19:13 +0530

    to print all math function file added

---
+++ Anil/mathfun.c
@@ -0,0 +1,15 @@
+    //program to use all math function.
+    #include<stdio.h>  
+    #include <math.h>    
+    int main(){    
+        printf("\n%f",ceil(3.6));    
+        printf("\n%f",ceil(3.3));    
+        printf("\n%f",floor(3.6));    
+        printf("\n%f",floor(3.2));    
+        printf("\n%f",sqrt(16));    
+        printf("\n%f",sqrt(7));    
+        printf("\n%f",pow(2,4));    
+        printf("\n%f",pow(3,3));    
+        printf("\n%d",abs(-12));     
+     return 0;    
+    }    
