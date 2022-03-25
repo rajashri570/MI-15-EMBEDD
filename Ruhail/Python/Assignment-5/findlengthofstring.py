526f0c6b8197c40a973549bee119f69b0deeb5e9
Ruhailkhan2016 <ruhail.tca1709079@tmu.ac.in>
2022-03-25 14:47:06 +0530

    Add Assignment-5

---
+++ Ruhail/Python/Assignment-5/findlengthofstring.py
@@ -0,0 +1,11 @@
+# Program 4 : Find length of a string in python using for loop
+
+str = input("input any string : ")
+
+cal = 0
+
+for i in str:
+    
+    cal = cal+1
+    
+print("Length of String is : ",cal)
